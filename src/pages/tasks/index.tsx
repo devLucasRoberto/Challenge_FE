948a78f28d4346989dd4c495e981b18b05410b0e
pedro francis <luucasroberto250@gmail.com>
2022-04-10 02:22:19 +0100

    add table

--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -1,3 +1,32 @@
+import {
+  Paper,
+  Table,
+  TableHead,
+  TableContainer,
+  TableRow,
+  TableCell,
+  TableBody
+} from '@mui/material'
+
 export function Tasks() {
-  return <h1>tasks</h1>
+  return (
+    <TableContainer component={Paper} sx={{ bgcolor: 'primary.main' }}>
+      <Table arial-label="tasks table" stickyHeader>
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell>Task</TableCell>
+            <TableCell>Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+            <TableCell>1</TableCell>
+            <TableCell>task 1</TableCell>
+            <TableCell>true</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
 }
