bb02f73805a4b68c34e81d59f46140ab0f1011a3
pedro francis <luucasroberto250@gmail.com>
2022-04-10 18:02:40 +0100

    removed react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
 import { Box, CssBaseline, ThemeProvider } from '@mui/material'
-import { QueryClientProvider } from 'react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
 import { SidebarDrawerProvider } from './contexts/SidebarDrawerContext'
 import { Index } from './pages'
 import { Tasks } from './pages/tasks'
-import { queryClient } from './services/queryClient'
 import { theme } from './styles/theme'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <SidebarDrawerProvider>
-            <Header />
+        <SidebarDrawerProvider>
+          <Header />
 
-            <Box display="flex">
-              <Sidebar />
+          <Box display="flex">
+            <Sidebar />
 
-              <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="tasks" element={<Tasks />} />
-              </Routes>
-            </Box>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="tasks" element={<Tasks />} />
+            </Routes>
+          </Box>
 
-            <CssBaseline />
-          </SidebarDrawerProvider>
-        </QueryClientProvider>
+          <CssBaseline />
+        </SidebarDrawerProvider>
       </BrowserRouter>
     </ThemeProvider>
   )
