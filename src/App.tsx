8ed2fe142fa4498b89605dfd3b59f22da36bb834
pedro francis <luucasroberto250@gmail.com>
2022-04-10 17:15:30 +0100

    add axios and react-query provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
 import { Box, CssBaseline, ThemeProvider } from '@mui/material'
+import { QueryClientProvider } from 'react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
 import { SidebarDrawerProvider } from './contexts/SidebarDrawerContext'
 import { Index } from './pages'
 import { Tasks } from './pages/tasks'
+import { queryClient } from './services/queryClient'
 import { theme } from './styles/theme'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <SidebarDrawerProvider>
-          <Header />
+        <QueryClientProvider client={queryClient}>
+          <SidebarDrawerProvider>
+            <Header />
 
-          <Box display="flex">
-            <Sidebar />
+            <Box display="flex">
+              <Sidebar />
 
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="tasks" element={<Tasks />} />
-            </Routes>
-          </Box>
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="tasks" element={<Tasks />} />
+              </Routes>
+            </Box>
 
-          <CssBaseline />
-        </SidebarDrawerProvider>
+            <CssBaseline />
+          </SidebarDrawerProvider>
+        </QueryClientProvider>
       </BrowserRouter>
     </ThemeProvider>
   )
